544eccfeaed48d5e79c22525a3544e5f3b7a9012
taragopal <67571877+taragopal@users.noreply.github.com>
2025-09-14 17:19:55 +0200

    Create 04_list_method_reverse_sort.py

---
+++ 04_list_method_reverse_sort.py
@@ -0,0 +1,13 @@
+array = ['a', 'b', 'c', 'x', 'm', 'o', 'f', 'i', 4, 5]
+print(array)
+
+print(array[::-1])  #reverse the list
+array_100 = print(list(range(1, 101)))
+array_11 = print(list(range(11)))
+
+sentence = ' - '
+new_sentence = sentence.join(['hi', 'my', 'dogs'
+                              'name', 'is', 'Mehul'])
+work = ' '.join(['I', 'work', 'for', 'Pictet', 'in', 'Geneva', '.'])
+print(new_sentence)
+print(work)
