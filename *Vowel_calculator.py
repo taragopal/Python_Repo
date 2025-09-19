679fcf0cd50d7239b22b2acda00cbb6f2b2972d5
taragopal <67571877+taragopal@users.noreply.github.com>
2025-09-19 21:22:18 +0200

    Create *Vowel_calculator.py

---
+++ *Vowel_calculator.py
@@ -0,0 +1,19 @@
+for n in range(1, 5):
+  print(n)
+
+for letter in ['a', 'b', 'c', 'd']:
+  print(letter)
+
+vowels = 0
+consonants = 0
+#initialize the counters
+l = input(
+    "Enter a word, and I will tell you the count of vowel and consonants in it: "
+).strip().lower()
+
+if letter.l in "aeiou":
+  vowels += 1
+else:
+  consonants += 1
+
+print(f'There are {vowels} vowels and {consonants} consonants in the word {l}')
