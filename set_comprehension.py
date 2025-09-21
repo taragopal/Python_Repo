fe2b12111c6e85391e8e338d672662e17ebb06d7
taragopal <67571877+taragopal@users.noreply.github.com>
2025-09-21 23:54:32 +0200

    Create set_comprehension.py

---
+++ set_comprehension.py
@@ -0,0 +1,22 @@
+print("--List values that appearing more than once ----")
+
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+duplicates = []
+for values in some_list:
+    if some_list.count(values) > 1:
+        if values not in duplicates:
+            duplicates.append(values)
+print(duplicates)
+
+#Test using comprehensions
+#convert the above code into a list comprehension
+#convert the set to a list
+#iterate over the list and count the number of times the item appears in the list
+#update the list with the items that appear more than once
+
+
+print("---------Test Using comprehensions shortcuts -----------")
+
+duplicates = list(set([items for items in some_list if some_list.count(items) > 1]))
+print (duplicates) 
+
