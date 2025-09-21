cfefef2aebd2a2da46748450a736638e20c33acc
taragopal <67571877+taragopal@users.noreply.github.com>
2025-09-21 19:14:31 +0200

    Update if_else_magician.py

--- if_else_magician.py
+++ if_else_magician.py
@@ -1,11 +1,14 @@
 #create a if else loop with logical operators
+#use with input statements 
 
 is_magician = True
 is_expert =  True
 
 if is_expert and is_magician:  
     print("You are a master magician")
+
 elif is_magician and not is_expert:
     print("At least you're getting there")
+
 elif not is_magician:
     print("You need magic powers")
